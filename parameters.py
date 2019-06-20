e312bbc64aa471ece673f464e01a8bd329796dcd
Daniel Leal <dlleal@uc.cl>
2019-06-20 01:39:06 -0400

    Now the parameters are loaded from json

--- parameters.py
+++ parameters.py
@@ -3,4 +3,12 @@ This module contains the necessary parameters to make the mailer work.
 """
 
 
+import json
+
+
+# Para usar el mailer, rellenar con el mail a usar
 MAIL_SENDER = ''
+
+# Para usar el mailer, comentar las siguientes lineas
+with open('mail.json', 'r') as metadata:
+    MAIL_SENDER = json.load(metadata)['mail']
